5e8627b9bb18d95d1c49e60f85ed2f6b9e0b7bc9
supercode0228 <bee0228angel@outlook.com>
2020-07-31 10:55:39 -0400

    Sent the message when enter key

--- src/components/ChatRoom/MessageInput.js
+++ src/components/ChatRoom/MessageInput.js
@@ -1,58 +1,61 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-import {
-	InputGroup,
-	InputGroupAddon,
-	Input,
-	Button
-} from 'reactstrap';
+import { InputGroup, InputGroupAddon, Input, Button } from 'reactstrap'
 
 class MessageInput extends Component {
-	constructor() {
-		super();
-		this.state={
-			message: '',
-		}
-	}
+  constructor() {
+    super()
+    this.state = {
+      message: '',
+    }
+  }
 
-	onChange = (e) => {
-		this.setState({
-			[e.target.name]: e.target.value
-		})
-	}
+  onKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      this.props.onSendMsg(e, this.state.message)
+      this.setState({ message: '' })
+    }
+  }
 
-	onSendMsg = (e, message) => {
-		this.setState({
-			message: ''
-		});
-		this.props.onSendMsg(e, message);
-	}
+  onChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+  }
 
-	render() {
-		const { message } = this.state;
+  onSendMsg = (e, message) => {
+    this.setState({
+      message: '',
+    })
+    this.props.onSendMsg(e, message)
+  }
 
-		return (
-			<div className="chat-room__message__type">
-				<InputGroup>
-					<Input
-						name="message"
-						className="chat-room__message__type__input"
-						placeholder="Type your message..."
-						onChange={this.onChange}
-						value={message}
-					/>
-					<InputGroupAddon addonType="append">
-						<Button
-							className="chat-room__message__type__send"
-							onClick={(e) => this.onSendMsg(e, message)}
-						>
-							Send
-						</Button>
-					</InputGroupAddon>
-				</InputGroup>
-			</div>
-		)
-	}
+  render() {
+    const { message } = this.state
+
+    return (
+      <div className="chat-room__message__type">
+        <InputGroup>
+          <Input
+            name="message"
+            className="chat-room__message__type__input"
+            placeholder="Type your message..."
+            onChange={this.onChange}
+            onKeyPress={this.onKeyPress}
+            value={message}
+          />
+          <InputGroupAddon addonType="append">
+            <Button
+              className="chat-room__message__type__send"
+              onClick={(e) => this.onSendMsg(e, message)}
+            >
+              Send
+            </Button>
+          </InputGroupAddon>
+        </InputGroup>
+      </div>
+    )
+  }
 }
 
-export default MessageInput;
+export default MessageInput
